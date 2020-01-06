58157f6b5d4d54768438796ce0426cbc82fbe3a6
justwe7 <ilihuaxi@gmail.com>
2020-01-06 19:27:34 +0800

    jenkins test

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var http = require("http");
+var url = require("url");
 
 http.createServer(function onReq(req, res) {
+  var pathname = url.parse(req.url).pathname;
   if (pathname === '/favicon.ico') {
     return;
   }
   res.writeHead(200, {"Content-Type": "text/plain"})
   const now = new Date()
-  res.write(`哦哈哈哈${now.getMinutes()}分-${now.getSeconds()}`)
+  res.write(`哦哈哈哈${now.getMinutes()}分：${now.getSeconds()}秒`)
   res.end()
 }).listen(7878)
