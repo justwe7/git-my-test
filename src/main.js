e2db9ea5a10d31b8d2702e506951928bf3ffdfeb
lihx <ilihuaxi@gmail.com>
2022-01-11 10:09:32 +0800

    feat: merge-a

--- src/main.js
+++ src/main.js
@@ -4,6 +4,22 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// mergea
+function fooBranchA (param) {
+  const obj = {
+    a: 1,
+    b: 2,
+    c: 3
+  }
+  for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+      const element = obj[key];
+      console.log(element)
+    }
+  }
+  console.log(obj)
+}
+
 new Vue({
   router,
   render: h => h(App)
