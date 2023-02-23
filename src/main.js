b7ec5b28a44c12d06c34bb012554065f777952fa
justwe7 <ilihuaxi@gmail.com>
2023-02-23 20:30:43 +0800

    rebase1
    rebase2

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-
+// rebase1
+// rebase2
 // mergea -- merge-b修改
 function fooBranchA (param) {
   const objB = {
